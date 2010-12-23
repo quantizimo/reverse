557ec2df7dd3854c3861d6d007ee7791741de646
David Anisman <david.anisman@gmail.com>
2010-12-22 16:05:18 -0800

    Initial commit.

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  @title = "Enter your text here:"
+  erb :home
+end
+
+post '/' do
+  @title = "Here is your reversed text:"
+  @reversed_text = params[:phrase].reverse
+  erb :reverse
+end
+
+get '/:phrase' do
+  @title = "Here's Your Reversed Text:"
+  @reversed_text = params[:phrase].reverse
+  erb :reverse
+end
+
